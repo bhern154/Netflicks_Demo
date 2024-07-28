3b33988738a5f8da4164cffa21a8291ab848302f
bhern154 <brian1010199@gmail.com>
2024-07-27 17:41:31 -0700

    added a loading screen

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = (os.environ.get('DATABASE_URL', 'postgresql:///netflicks_db'))
+app.config["SQLALCHEMY_DATABASE_URI"] = (os.environ.get('DATABASE_URL'))
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
